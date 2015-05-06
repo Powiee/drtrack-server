c537284ff732fb240628a79dc2ac5de51a2f23d8
Jack Hsieh <jack@jackhsieh.com>
2015-05-06 20:30:15 +0000

    Add update evacuee and fix delet

--- routes/evacuee.js
+++ routes/evacuee.js
@@ -47,15 +47,38 @@ module.exports.Router = function(Evacuee) {
         }
       });
     })
-    .delete('/', function(req, res) {
-      console.log(req.body.code);
-      if(req.body.code) {
-        Evacuee.findOneAndRemove({code: req.body.code}, function(err, data) {
-          res.sendStatus(200);
-        });
-      } else {
-        res.sendStatus(400);
-      }
+    .put('/', function(req, res) {
+      Evacuee.findOneAndUpdate({code: req.body.code}, 
+      {
+        firstName: req.body.firstName,
+        middleName: req.body.middleName,
+        lastName: req.body.lastName,
+        passport: req.body.passport,
+        driverLic: req.body.driverLic,
+        ssn: req.body.ssn,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        nationality: req.body.nationality,
+        majorCategory: req.body.majorCategory,
+        minorCategory: {
+          pregnant: req.body.pregnant,
+          under: req.body.under
+        },
+        militaryAfflicated: req.body.militaryAfflicated,
+        code: req.body.code,
+        bloodType: req.body.bloodType,
+        weight: req.body.weight,
+        specialNeeds: req.body.specialNeeds
+      } , function(err, data) {
+        console.log(data);
+        res.sendStatus(200);
+      });
+    })
+    .delete('/:id', function(req, res) {
+      Evacuee.findByIdAndRemove(req.params.id, function(err, data) {
+        if(err) res.sendStatus(400);
+        res.sendStatus(200);
+      });
     })
     .get('/:code', function(req, res) {
       Evacuee.find({code: req.params.code}, function(err, data) {
